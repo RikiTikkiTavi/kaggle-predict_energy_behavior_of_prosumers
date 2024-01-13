f8cc42a31e83416c8642bbebe4934996d0857c57
RikiTikkiTavi <yahor.paromau@gmail.com>
2024-01-13 20:18:11 +0100

    Work on modelling

--- src/predict_energy_behavior/models/solar_output_regressor.py
+++ src/predict_energy_behavior/models/solar_output_regressor.py
@@ -60,6 +60,14 @@ class SolarOutputRegressor:
         """
         return abs(temperature - dewpoint) < threshold
 
+    def _rain_factor(self, parameters: dict[str, float], regressors: pd.DataFrame) -> np.ndarray:
+        rain_factor = 1.0
+        if "C_rain" in parameters:
+            rain_factor = np.maximum(0, 1 - parameters["C_rain"] * regressors["rain"])
+        elif "C_rain_1d" in parameters:
+            rain_factor = np.maximum(0, 1 + parameters["C_rain_1d"] * regressors["C_rain_1d"])
+        return rain_factor
+
     def predict(self, X: pd.DataFrame) -> np.ndarray:
         regressors = X
         baseline_temperature = self.baseline_temperature
@@ -67,58 +75,75 @@ class SolarOutputRegressor:
         parameters = self.weights
 
         # rain impact factor
-        rain_factor = np.maximum(0, 1 - parameters["C_rain"] * regressors["rain"])
+        rain_factor = self._rain_factor(parameters, regressors)
+        
         # snow impact factor
-        snow_partial_cov_factor = np.maximum(0, 1 - parameters["C_snow"] * regressors["snowfall"])
-        snow_factor = np.where(regressors["snowfall"] > parameters["Thr_snow_cov_100"], 0.0, snow_partial_cov_factor)
-        #snow_factor = np.maximum(0, 1 - parameters["C_snow"] * regressors["snowfall"])
+        snow_factor = 1.0
+        if "C_snow" in parameters:
+            snow_partial_cov_factor = np.maximum(0, 1 - parameters["C_snow"] * regressors["snowfall"])
+            snow_factor = np.where(regressors["snowfall"] > parameters["Thr_snow_cov_100"], 0.0, snow_partial_cov_factor)
+
         # wind impact factor
-        wind_factor = 1 + parameters["C_wind"] * regressors["windspeed_10m"]
+        wind_factor = 1.0
+        if "C_wind" in parameters:
+            wind_factor = 1 + parameters["C_wind"] * regressors["windspeed_10m"]
 
         # Adjust for Fog
-        fog_factor = np.where(
-            self._is_foggy(regressors["dewpoint"], regressors["temperature"], parameters["Thr_foggy"]),
-            parameters["C_fog"],
-            1 
-        )
+        fog_factor = 1.0
+        if "C_fog" in parameters:
+            fog_factor = np.where(
+                self._is_foggy(regressors["dewpoint"], regressors["temperature"], parameters["Thr_foggy"]),
+                parameters["C_fog"],
+                1 
+            )
 
         # Adjust for Dew in the Morning
-        dew_factor = np.where(
-            np.logical_and(regressors["hour"] > 6, regressors["hour"] < 9),
-            parameters["C_dew"],
-            1
-        )
+        dew_factor = 1.0
+        if "C_dew" in parameters:
+            dew_factor = np.where(
+                np.logical_and(regressors["hour"] > 6, regressors["hour"] < 9),
+                parameters["C_dew"],
+                1
+            )
 
         I_adj_total = 0.0
         # adjusted total horizontal irradiance
-        if "shortwave_radiation" in regressors:
+        if "shortwave_radiation" in regressors and "C_cloud_total" in parameters:
             I_adj_total = regressors["shortwave_radiation"] * (1 - parameters["C_cloud_total"] * regressors['cloudcover_total'] / 100)
         
+        # Direct and diffuse radiation adjusted to cloud cover
         I_adj_cc = 0.0
-        if "C_CC_total_direct" in parameters:
-            rad_to_reg = {"direct": "direct_solar_radiation", "diffuse": "diffuse_radiation"}
-            for rad in ["direct", "diffuse"]:
-                for cc in ["total", "high", "mid", "low"]:
+        rad_to_reg = {"direct": "direct_solar_radiation", "diffuse": "diffuse_radiation"}
+        for rad in ["direct", "diffuse"]:
+            for cc in ["total", "high", "mid", "low"]:
+                if f"C_CC_{cc}_{rad}" in parameters:
                     I_adj_cc += regressors[rad_to_reg[rad]] * (1 - parameters[f"C_CC_{cc}_{rad}"] * regressors[f'cloudcover_{cc}'] / 100) 
-
-        else:
-            # adjusted direct irradiance
-            I_adj_cc  += regressors["direct_solar_radiation"] * (1 - parameters["C_cloud_direct"] * regressors['cloudcover_high'] / 100)
-            # adjusted diffusive irradiance
-            I_adj_cc += regressors["diffuse_radiation"] * (1 - parameters["C_cloud_diffusive"] * regressors['cloudcover_low'] / 100)
         
         I_adj = I_adj_total + I_adj_cc
 
         E = I_adj / 1000
 
-        # effect of temperature
-        temperature_eff = 1 + parameters["C_temperature"] * (regressors["temperature"] - baseline_temperature)
-        temperature_eff[temperature_eff < 1] = 1
+        # effect of temperature above STC
+        temperature_eff = 1.0
+        if "C_temperature" in parameters:
+            temperature_eff = 1 + parameters["C_temperature"] * (regressors["temperature"] - baseline_temperature)
+            temperature_eff[temperature_eff < 1] = 1
+
+
+        # effect of temperature below STC
+        temperature_low_factor = 1.0
+        if "C_temperature_low" in parameters:
+            temperature_low_factor = 1 + parameters["C_temperature_low"] * np.abs(baseline_temperature - regressors["temperature"])**(1/2)
+            temperature_low_factor[temperature_low_factor > 1] = 1.0
         
         # total effectivity
         eta_total = baseline_efficiency * rain_factor * snow_factor * wind_factor * temperature_eff * fog_factor * dew_factor
 
-        return parameters["C_area"] * regressors["installed_capacity"] * E * eta_total
+        C_area = 1.0
+        if "C_area" in parameters:
+            C_area = parameters["C_area"]
+
+        return C_area * regressors["installed_capacity"] * E * eta_total
     
 
     def fit(self, X: pd.DataFrame, y: np.ndarray[float]) -> "SolarOutputRegressor" | NoReturn:
