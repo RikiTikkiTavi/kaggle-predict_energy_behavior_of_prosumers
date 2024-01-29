c7dbf2673bd5c945cd489e23e258e859396d3b64
RikiTikkiTavi <yahor.paromau@gmail.com>
2024-01-29 01:00:20 +0100

    w

--- src/predict_energy_behavior/models/production/two_orders_regression.py
+++ src/predict_energy_behavior/models/production/two_orders_regression.py
@@ -105,3 +105,87 @@ class TwoOrdersRegression(ProductionRegressionBase[Literal[2]]):
     def save(self, path: Path):
         self._first_order_model.save(path / "production_1.pickle")
         self._second_order_model.save(path)
+
+
+class AvgTwoOrdersRegression(TwoOrdersRegression):
+    def __init__(
+        self,
+        first_order_model: LGBMSecondOrderModel,
+        second_order_model: SecondOrderModel,
+        weights: tuple[float, float] = (0.5, 0.5)
+    ) -> None:
+        self._first_order_model = first_order_model
+        self._second_order_model = second_order_model
+        self.weights= weights
+
+    def get_model(self, order: int):
+        assert 0 < order and order < 3
+        if order == 1:
+            return self._first_order_model
+        else:
+            return self._second_order_model
+
+    def predict(self, X: pd.DataFrame):
+        X = X.copy()
+
+        results_first_order = self._first_order_model.predict(X)
+        X["predictions_first_order"] = results_first_order
+
+        return self.weights[0] * results_first_order + self.weights[1] * self._second_order_model.predict(X)
+
+    def _fit_with_separate_dfs(self, train_tups: tuple[TrainTupType, TrainTupType]):
+        d_1, d_2 = train_tups
+        
+        _logger.info("Fit 1 order ...")
+        self._first_order_model.fit(*d_1)
+        # _logger.info(f"Fit 1 order model:\n{str(self._first_order_model)}")
+
+        X = d_2[0].copy()
+        results_first_order = self._first_order_model.predict(X)
+        X["predictions_first_order"] = results_first_order
+
+        _logger.info("Fit 2 order ...")
+        self._second_order_model.fit(X, d_2[1])
+
+        return self
+
+    def _fit_with_same_df(self, X: pd.DataFrame, y: np.ndarray):
+        X = X.copy()
+
+        _logger.info("Fit 1 order ...")
+        self._first_order_model.fit(X, y)
+        _logger.info(f"Fit 1 order model:\n{str(self._first_order_model)}")
+
+        results_first_order = self._first_order_model.predict(X)
+
+        X["predictions_first_order"] = results_first_order
+
+        _logger.info("Fit 2 order ...")
+        self._second_order_model.fit(X, y)
+
+        return self
+
+    @overload
+    def fit(self, train_tups: tuple[TrainTupType, TrainTupType]) -> Self:
+        ...
+
+    @overload
+    def fit(self, X: pd.DataFrame, y: np.ndarray) -> Self:
+        ...
+
+    def fit(self, **kwargs) -> Self:
+        if "train_tups" in kwargs:
+            return self._fit_with_separate_dfs(**kwargs)
+        else:
+            return self._fit_with_same_df(**kwargs)
+
+    @classmethod
+    def load(cls, path: Path) -> "TwoOrdersRegression":
+        return TwoOrdersRegression(
+            first_order_model=GroupedSolarOutputRegression.load(path / "production_1.pickle"),
+            second_order_model=LGBMSecondOrderModel.load(path)
+        )
+
+    def save(self, path: Path):
+        self._first_order_model.save(path / "production_1.pickle")
+        self._second_order_model.save(path)
